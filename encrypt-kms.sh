7e7eb16f64a81c96b55d4df9ad056e1ac23edbd1
Weiming Li <weiming.li@novatop.net>
2019-04-26 00:21:16 +0800

    updated profile and key-id

--- encrypt-kms.sh
+++ encrypt-kms.sh
@@ -2,18 +2,18 @@
 
 echo "This is a message" > text.txt
 
-keyid="1b0c33b7-6543-4c61-8974-1db8139b627b"
+keyid="6b016347-5ecd-4bdf-8b3a-92f2b99d1758"
 
 echo "Generating data key from AWS KMS..."
-keys=($(aws kms generate-data-key --key-id $keyid --key-spec AES_256 --query [Plaintext,CiphertextBlob] --output text --profile global))
+keys=($(aws kms generate-data-key --key-id $keyid --key-spec AES_256 --query [Plaintext,CiphertextBlob] --output text --profile aws-global))
 
 #Store plaintext data key
 key=$(echo -n ${keys[0]} | base64 --decode | hexdump -v -e '/1 "%02X"')
 
 echo "Generating random initialization vector..."
-aws kms generate-random --number-of-bytes 16 --profile global
+aws kms generate-random --number-of-bytes 16 --profile aws-global
 
-iv=$(aws kms generate-random --number-of-bytes 16 --query Plaintext --output text --profile global | base64 --decode | hexdump -v -e '/1 "%02X"')
+iv=$(aws kms generate-random --number-of-bytes 16 --query Plaintext --output text --profile aws-global | base64 --decode | hexdump -v -e '/1 "%02X"')
 
 echo "Encrypted data key: ${keys[1]}" > encrypted.txt
 
